441a80e270e573332e0a52ae9ccdc88117c3bd22
Bruno Galvao <bruno_galvao@outlook.com>
2022-10-12 11:00:09 -0300

    add dir

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import '../css/style.css'
+import javascriptLogo from '../img/javascript.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
