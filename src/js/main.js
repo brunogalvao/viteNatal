ec992c2a04b2096be1958a4d6ae0e9f25658df4d
Bruno Galvao <bruno_galvao@outlook.com>
2022-10-17 16:42:45 -0300

    scroll 2500

--- src/js/main.js
+++ src/js/main.js
@@ -54,7 +54,7 @@ if (pinnedImageWrappers) {
           snap: 1 / (sections.length - 1),
           // start: "top top",
           // end: () => `+=${inner.offsetWidth}`,
-          end: '+=3000%'
+          end: '+=2500%'
       }
     });
 
